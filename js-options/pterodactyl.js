031521cbd256b2b4bafcabed83c6b3e3db1f309b
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-09-04 10:49:05 +0000

    Update pterodactyl.js

--- js-options/pterodactyl.js
+++ js-options/pterodactyl.js
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 export async function installPterodactyl(vps) {
   const cmd = `
     docker exec -it ${vps.containerName} bash -c "
-    curl -Lo panel.sh https://raw.githubusercontent.com/<ton_user>/blackhatvps-scripts/main/pterodactyl-install.sh &&
+    curl -Lo panel.sh https://raw.githubusercontent.com/LORD-OBITO-DEV/blackhatvps-scripts/main/pterodactyl-install.sh &&
     bash panel.sh
     "
   `;
